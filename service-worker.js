bc5e690392bff4cbd38e68fa48e95c063b546aab
manueltellechea38-cmd <manueltellechea38@gmail.com>
2026-08-26 23:09:34 -0300

    Corrige análisis y agrega exportación PDF PNG

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="escuchamapa-v3";
+const CACHE="escuchamapa-v4";
 const ASSETS=["./","./index.html","./css/styles.css","./js/app.js","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
